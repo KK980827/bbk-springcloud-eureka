f5daa2a97cf7273c27711462f93e424721312c46
KK980827 <KK980827@163.com>
2020-02-12 16:16:51 +0800

    更改包结构

--- src/main/java/com/bestbigkk/eureka/EurekaServer.java
+++ src/main/java/com/bestbigkk/eureka/EurekaServer.java
@@ -1,4 +1,4 @@
-package com.bestbigkk.verification.eureka;
+package com.bestbigkk.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
